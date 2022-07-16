d6cc165171382c80389c66c02b6e28e1904df4f0
AbdirahmanAbdi <abdirahmanabdi.ma@gmail.com>
2022-07-16 16:00:46 +0300

    your message

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
-
 import { CATEGORIES, TASKS } from "../data";
 console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
-  return (
+  const [tasks, setTask] =useState(TASKS)
+  const [selectedCategoty, setSelectedCategory] = useState("All")
+
+  function removeTask(taskToRemove){
+    const newList = tasks.filter((todo) => todo.text !==taskToRemove)
+    setTask(newList);
+  }
+  const changeCategories = (category)=>{
+    setSelectedCategory(category)
+  } 
+  const filteredTest =tasks.filter((task)=>task.category === selectedCategoty || selectedCategoty ==="All")
+
+  function onTaskFormSubmit(newTask){
+    setTask([...tasks, newTask])
+  }
+  return ( 
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter categories={CATEGORIES} changeCategories={changeCategories} selectedCategoty ={selectedCategoty}/>
+      <NewTaskForm categories={CATEGORIES.filter(cat => cat !== "All")} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={filteredTest} removeTask={removeTask}/>
     </div>
   );
 }
